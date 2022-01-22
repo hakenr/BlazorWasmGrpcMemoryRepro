90e81b0fda7c86ec4333b9b746555a6e0515bcea
Robert Haken <haken@havit.cz>
2022-01-22 23:07:34 +0100

    .NET5 downgrade

--- WasmGrpcErrorRepro/Client/Program.cs
+++ WasmGrpcErrorRepro/Client/Program.cs
@@ -7,24 +7,31 @@ using ProtoBuf.Grpc.Client;
 using WasmGrpcErrorRepro.Client;
 using WasmGrpcErrorRepro.Shared;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddSingleton(sp =>
+public class Program
 {
-  var baseUri = sp.GetRequiredService<NavigationManager>().BaseUri;
-  var handler = new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler());
-  return GrpcChannel.ForAddress(baseUri,
-    new GrpcChannelOptions() { HttpClient = new HttpClient(handler) });
-});
+	public static async Task Main(string[] args)
+	{
 
-builder.Services.AddScoped(sp =>
-{
-  var channel = sp.GetRequiredService<GrpcChannel>();
-  return channel.CreateGrpcService<IDataService>();
-});
+		var builder = WebAssemblyHostBuilder.CreateDefault(args);
+		builder.RootComponents.Add<App>("#app");
+		//builder.RootComponents.Add<HeadOutlet>("head::after");
 
-await builder.Build().RunAsync();
+		builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+		builder.Services.AddSingleton(sp =>
+		{
+			var baseUri = sp.GetRequiredService<NavigationManager>().BaseUri;
+			var handler = new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler());
+			return GrpcChannel.ForAddress(baseUri,
+	  new GrpcChannelOptions() { HttpClient = new HttpClient(handler) });
+		});
+
+		builder.Services.AddScoped(sp =>
+		{
+			var channel = sp.GetRequiredService<GrpcChannel>();
+			return channel.CreateGrpcService<IDataService>();
+		});
+
+		await builder.Build().RunAsync();
+	}
+}
