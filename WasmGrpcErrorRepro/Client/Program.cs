5d89345f9ab541f9f7348afc2a2329f2d3bcfcbe
Robert Haken <haken@havit.cz>
2022-02-16 18:48:56 +0100

    grpc Channel transient

--- WasmGrpcErrorRepro/Client/Program.cs
+++ WasmGrpcErrorRepro/Client/Program.cs
@@ -13,7 +13,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddSingleton(sp =>
+builder.Services.AddTransient(sp =>
 {
   var baseUri = sp.GetRequiredService<NavigationManager>().BaseUri;
   var handler = new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler());
