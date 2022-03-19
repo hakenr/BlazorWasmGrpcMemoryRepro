ad8a1deef2618ce7489f1a9d5aff1cf0c11b4f7b
Robert Haken <haken@havit.cz>
2022-03-19 22:25:56 +0100

    gRPC ClientFactory with interceptor

---
+++ WasmGrpcErrorRepro/Client/GrpcSingleCallInterceptor.cs
@@ -0,0 +1,44 @@
+﻿using Grpc.Core;
+using Grpc.Core.Interceptors;
+
+namespace WasmGrpcErrorRepro.Client
+{
+	public class GrpcSingleCallInterceptor : Interceptor
+	{
+		public GrpcSingleCallInterceptor()
+		{
+			Console.WriteLine("GrpcSingleCallInterceptor:ctor");
+		}
+
+		public override AsyncUnaryCall<TResponse> AsyncUnaryCall<TRequest, TResponse>(
+					TRequest request,
+					ClientInterceptorContext<TRequest, TResponse> context,
+					AsyncUnaryCallContinuation<TRequest, TResponse> continuation)
+		{
+			Console.WriteLine("AsyncUnaryCall_Start");
+			var call = continuation(request, context);
+			Console.WriteLine("AsyncUnaryCall_Start_postCall");
+
+			return new AsyncUnaryCall<TResponse>(HandleResponseAsync(call.ResponseAsync), call.ResponseHeadersAsync, call.GetStatus, call.GetTrailers, call.Dispose);
+		}
+
+		private readonly SemaphoreSlim callLock = new SemaphoreSlim(1, 1);
+		private async Task<TResponse> HandleResponseAsync<TResponse>(Task<TResponse> responseTask)
+		{
+			Console.WriteLine($"GrpcSingleCallInterceptor_pre:callLock.CurrentCount={callLock.CurrentCount}");
+			await callLock.WaitAsync();
+			Console.WriteLine($"GrpcSingleCallInterceptor_post:callLock.CurrentCount={callLock.CurrentCount}");
+			try
+			{
+				Console.WriteLine("GrpcSingleCallInterceptor:HandleResponseAsync");
+				return await responseTask;
+			}
+			finally
+			{
+				Console.WriteLine($"GrpcSingleCallInterceptor_Release_pre:callLock.CurrentCount={callLock.CurrentCount}");
+				callLock.Release();
+				Console.WriteLine($"GrpcSingleCallInterceptor_Release_post:callLock.CurrentCount={callLock.CurrentCount}");
+			}
+		}
+	}
+}
--- WasmGrpcErrorRepro/Client/Program.cs
+++ WasmGrpcErrorRepro/Client/Program.cs
@@ -3,7 +3,9 @@ using Grpc.Net.Client.Web;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using ProtoBuf.Grpc.Client;
+using ProtoBuf.Grpc.ClientFactory;
+using ProtoBuf.Grpc.Configuration;
+using ProtoBuf.Meta;
 using WasmGrpcErrorRepro.Client;
 using WasmGrpcErrorRepro.Shared;
 
@@ -13,6 +15,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddTransient<GrpcSingleCallInterceptor>();
+
 builder.Services.AddSingleton(sp =>
 {
   var baseUri = sp.GetRequiredService<NavigationManager>().BaseUri;
@@ -21,10 +25,24 @@ builder.Services.AddSingleton(sp =>
     new GrpcChannelOptions() { HttpClient = new HttpClient(handler) });
 });
 
-builder.Services.AddScoped(sp =>
+//builder.Services.AddScoped(sp =>
+//{
+//  var channel = sp.GetRequiredService<GrpcChannel>();
+//	return channel.CreateGrpcService<IDataService>();
+//});
+
+builder.Services.AddTransient<GrpcWebHandler>(provider => new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
+builder.Services.AddSingleton<ClientFactory>(ClientFactory.Create());
+
+
+builder.Services.AddCodeFirstGrpcClient<IDataService>((provider, options) =>
 {
-  var channel = sp.GetRequiredService<GrpcChannel>();
-  return channel.CreateGrpcService<IDataService>();
-});
+	var navigationManager = provider.GetRequiredService<NavigationManager>();
+	var backendUrl = navigationManager.BaseUri;
+
+	options.Address = new Uri(backendUrl);
+})
+	.ConfigurePrimaryHttpMessageHandler<GrpcWebHandler>()
+	.AddInterceptor<GrpcSingleCallInterceptor>();
 
 await builder.Build().RunAsync();
